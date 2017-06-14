85359ef5abc6dd7ca1ec83483f1951dd428f556b
Peter Johnson <insomnia@rcpt.at>
2017-06-14 13:22:23 +0200

    vendor: update gosmparse, disable PBF indexing, moved to pbf_indexing branch on missinglink fork

--- vendor/github.com/thomersch/gosmparse/decoder.go
+++ vendor/github.com/thomersch/gosmparse/decoder.go
@@ -6,10 +6,8 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
 	"runtime"
 	"sync"
-	"sync/atomic"
 
 	"github.com/thomersch/gosmparse/OSMPBF"
 
@@ -21,53 +19,20 @@ type Decoder struct {
 	// QueueSize allows to tune the memory usage vs. parse speed.
 	// A larger QueueSize will consume more memory, but may speed up the parsing process.
 	QueueSize int
-	r         *os.File
+	r         io.Reader
 	o         OSMReader
-	Mutex     *sync.Mutex
-	BytesRead uint64
-	Index     *BlobIndex
 }
 
 // NewDecoder returns a new decoder that reads from r.
-func NewDecoder(r *os.File) *Decoder {
+func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{
 		r:         r,
 		QueueSize: 200,
 	}
 }
 
-// ParseBlob - parse a single blob
-func (d *Decoder) ParseBlob(o OSMReader, offset int64) error {
-
-	// ---
-	d.Index = &BlobIndex{}
-	d.Mutex = &sync.Mutex{}
-
-	d.o = o
-	d.r.Seek(offset, 0)
-
-	_, blob, err := d.block()
-	if err != nil {
-		if err == io.EOF {
-			return nil
-		}
-		return err
-	}
-
-	err = d.readElements(blob)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // Parse starts the parsing process that will stream data into the given OSMReader.
 func (d *Decoder) Parse(o OSMReader) error {
-
-	d.Index = &BlobIndex{}
-	d.Mutex = &sync.Mutex{}
-
 	d.o = o
 	header, _, err := d.block()
 	if err != nil {
@@ -126,53 +91,26 @@ func (d *Decoder) Parse(o OSMReader) error {
 }
 
 func (d *Decoder) block() (*OSMPBF.BlobHeader, *OSMPBF.Blob, error) {
-
-	// store info
-	var startBytes = d.BytesRead
-
-	var byteCount int
-	var err error
-
 	// BlobHeaderLength
 	headerSizeBuf := make([]byte, 4)
-
-	// read BlobHeaderLength
-	byteCount, err = io.ReadFull(d.r, headerSizeBuf)
-
-	// keep track of bytes read so far
-	atomic.AddUint64(&d.BytesRead, uint64(byteCount))
-
-	// error checking
-	if err != nil {
+	if _, err := io.ReadFull(d.r, headerSizeBuf); err != nil {
 		return nil, nil, err
 	}
 	headerSize := binary.BigEndian.Uint32(headerSizeBuf)
 
 	// BlobHeader
 	headerBuf := make([]byte, headerSize)
-
-	// read BlobHeader
-	byteCount, err = io.ReadFull(d.r, headerBuf)
-
-	// keep track of bytes read so far
-	atomic.AddUint64(&d.BytesRead, uint64(byteCount))
-
-	if err != nil {
+	if _, err := io.ReadFull(d.r, headerBuf); err != nil {
 		return nil, nil, err
 	}
-
 	blobHeader := new(OSMPBF.BlobHeader)
-	if err = proto.Unmarshal(headerBuf, blobHeader); err != nil {
+	if err := proto.Unmarshal(headerBuf, blobHeader); err != nil {
 		return nil, nil, err
 	}
 
 	// Blob
 	blobBuf := make([]byte, blobHeader.GetDatasize())
-	byteCount, err = io.ReadFull(d.r, blobBuf)
-
-	// keep track of bytes read so far
-	atomic.AddUint64(&d.BytesRead, uint64(byteCount))
-
+	_, err := io.ReadFull(d.r, blobBuf)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -180,93 +118,26 @@ func (d *Decoder) block() (*OSMPBF.BlobHeader, *OSMPBF.Blob, error) {
 	if err := proto.Unmarshal(blobBuf, blob); err != nil {
 		return nil, nil, err
 	}
-
-	// store info
-	d.Mutex.Lock()
-	d.Index.Blobs = append(d.Index.Blobs, &BlobInfo{
-		Start: startBytes,
-		Size:  uint64(byteCount),
-	})
-
-	// hack to store the blob index key
-	var key = make([]byte, 8)
-	binary.LittleEndian.PutUint64(key, uint64(len(d.Index.Blobs)-1))
-	blob.XXX_unrecognized = key
-
-	d.Mutex.Unlock()
-
 	return blobHeader, blob, nil
 }
 
 func (d *Decoder) readElements(blob *OSMPBF.Blob) error {
-
 	pb, err := d.blobData(blob)
 	if err != nil {
 		return err
 	}
 
 	for _, pg := range pb.Primitivegroup {
-
-		var info = &GroupInfo{}
-
 		switch {
 		case pg.Dense != nil:
-
-			info.Type = "node"
-			info.Count = len(pg.Dense.Id)
-
-			// find high and low id
-			var id int64
-			for index := range pg.Dense.Id {
-				id = pg.Dense.Id[index] + id
-				if 0 == info.High || id > info.High {
-					info.High = id
-				}
-				if 0 == info.Low || id < info.Low {
-					info.Low = id
-				}
-			}
-
 			if err := denseNode(d.o, pb, pg.Dense); err != nil {
 				return err
 			}
 		case len(pg.Ways) != 0:
-
-			info.Type = "way"
-			info.Count = len(pg.Ways)
-
-			// find high and low id
-			var id int64
-			for _, way := range pg.Ways {
-				id = way.GetId()
-				if 0 == info.High || id > info.High {
-					info.High = id
-				}
-				if 0 == info.Low || id < info.Low {
-					info.Low = id
-				}
-			}
-
 			if err := way(d.o, pb, pg.Ways); err != nil {
 				return err
 			}
 		case len(pg.Relations) != 0:
-
-			info.Type = "relation"
-			info.Count = len(pg.Relations)
-
-			// find high and low id
-			var id int64
-			for _, way := range pg.Relations {
-				id = way.GetId()
-				if 0 == info.High || id > info.High {
-					info.High = id
-				}
-				if 0 == info.Low || id < info.Low {
-					info.Low = id
-				}
-			}
-
 			if err := relation(d.o, pb, pg.Relations); err != nil {
 				return err
 			}
@@ -275,17 +146,7 @@ func (d *Decoder) readElements(blob *OSMPBF.Blob) error {
 		default:
 			return fmt.Errorf("no supported data in primitive group")
 		}
-
-		d.Mutex.Lock()
-
-		// hack to retrieve the blob index key
-		var key = int(binary.LittleEndian.Uint64(blob.XXX_unrecognized))
-
-		d.Index.Blobs[key].Groups = append(d.Index.Blobs[key].Groups, info)
-		d.Mutex.Unlock()
-
 	}
-
 	return nil
 }
 
---
+++ vendor/github.com/thomersch/gosmparse/tags_test.go
@@ -0,0 +1,100 @@
+package gosmparse
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/facebookgo/ensure"
+)
+
+type mockedKVReader struct {
+	sync.RWMutex
+
+	nodes, ways, rels map[int64]map[string]string
+}
+
+func (r *mockedKVReader) ReadNode(n Node) {
+	r.Lock()
+	defer r.Unlock()
+	r.nodes[n.ID] = n.Tags
+}
+
+func (r *mockedKVReader) ReadWay(w Way) {
+	r.Lock()
+	defer r.Unlock()
+	r.ways[w.ID] = w.Tags
+}
+
+func (r *mockedKVReader) ReadRelation(rel Relation) {
+	r.Lock()
+	defer r.Unlock()
+	r.rels[rel.ID] = rel.Tags
+}
+
+func TestDenseNodeKV(t *testing.T) {
+	mr := mockedKVReader{
+		nodes: make(map[int64]map[string]string),
+	}
+
+	testFile, err := os.Open("testdata/node_kv.osm.pbf")
+	ensure.Nil(t, err)
+	buf, err := ioutil.ReadAll(testFile)
+	ensure.Nil(t, err)
+	reader := bytes.NewReader(buf)
+
+	dec := NewDecoder(reader)
+	err = dec.Parse(&mr)
+	ensure.Nil(t, err)
+	ensure.DeepEqual(t, mr.nodes, map[int64]map[string]string{
+		1: {"key1": "value1", "key2": "value2"},
+		2: {"key2": "value2_node2"},
+		3: {"key1": "value1_node3"},
+	})
+}
+
+func TestWaysKV(t *testing.T) {
+	mr := mockedKVReader{
+		nodes: make(map[int64]map[string]string),
+		ways:  make(map[int64]map[string]string),
+	}
+
+	testFile, err := os.Open("testdata/way_kv.osm.pbf")
+	ensure.Nil(t, err)
+	buf, err := ioutil.ReadAll(testFile)
+	ensure.Nil(t, err)
+	reader := bytes.NewReader(buf)
+
+	dec := NewDecoder(reader)
+	err = dec.Parse(&mr)
+	ensure.Nil(t, err)
+	ensure.DeepEqual(t, mr.ways, map[int64]map[string]string{
+		1: {"name": "line", "highway": "primary"},
+		2: {"highway": "primary", "foo": "bar"},
+		3: {"unlogical": "true", "width": "3", "name": "line"},
+	})
+}
+
+func TestRelationsKV(t *testing.T) {
+	mr := mockedKVReader{
+		nodes: make(map[int64]map[string]string),
+		ways:  make(map[int64]map[string]string),
+		rels:  make(map[int64]map[string]string),
+	}
+
+	testFile, err := os.Open("testdata/relation_kv.osm.pbf")
+	ensure.Nil(t, err)
+	buf, err := ioutil.ReadAll(testFile)
+	ensure.Nil(t, err)
+	reader := bytes.NewReader(buf)
+
+	dec := NewDecoder(reader)
+	err = dec.Parse(&mr)
+	ensure.Nil(t, err)
+	ensure.DeepEqual(t, mr.rels, map[int64]map[string]string{
+		1: {"natural": "water", "wikipedia": "trololol"},
+		2: {"unnatural": "water", "ref": "12", "name": "foobar"},
+	})
+}
