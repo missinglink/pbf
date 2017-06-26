bd2a7d7bd18157802a15aa9ecf85843bfd005025
Peter Johnson <insomnia@rcpt.at>
2017-06-26 17:33:53 +0200

    nquad: add nquad format (generic graph db format)

---
+++ command/nquad.go
@@ -0,0 +1,73 @@
+package command
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/missinglink/pbf/handler"
+	"github.com/missinglink/pbf/lib"
+	"github.com/missinglink/pbf/parser"
+	"github.com/missinglink/pbf/proxy"
+
+	"github.com/codegangsta/cli"
+)
+
+// Nquad cli command
+func Nquad(c *cli.Context) error {
+
+	// validate args
+	var argv = c.Args()
+	if len(argv) != 1 {
+		fmt.Println("invalid arguments, expected: {pbf}")
+		os.Exit(1)
+	}
+
+	// create parser
+	parser := parser.NewParser(argv[0])
+
+	// create parser handler
+	var handle = &handler.Nquad{
+		Mutex: &sync.Mutex{},
+	}
+
+	// check if a bitmask is to be used
+	var bitmaskPath = c.String("bitmask")
+
+	// not using a bitmask
+	if "" == bitmaskPath {
+
+		// Parse will block until it is done or an error occurs.
+		parser.Parse(handle)
+
+		return nil
+	}
+
+	// using a bitmask file
+
+	// bitmask file doesn't exist
+	if _, err := os.Stat(bitmaskPath); err != nil {
+		fmt.Println("bitmask file doesn't exist")
+		os.Exit(1)
+	}
+
+	// debug
+	fmt.Println("loaded bitmask:", bitmaskPath)
+
+	// read bitmask from disk
+	masks := lib.NewBitmaskMap()
+	masks.ReadFromFile(bitmaskPath)
+
+	// create filter proxy
+	filter := &proxy.WhiteList{
+		Handler:      handle,
+		NodeMask:     masks.Nodes,
+		WayMask:      masks.Ways,
+		RelationMask: masks.Relations,
+	}
+
+	// Parse will block until it is done or an error occurs.
+	parser.Parse(filter)
+
+	return nil
+}
