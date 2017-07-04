839b355d24b9b957e81c31447e378afbf2b2dd0e
missinglink <insomnia@rcpt.at>
2017-07-04 21:56:02 +0200

    xcompile: strip local paths from panic stack traces

--- xcompile.sh
+++ xcompile.sh
@@ -39,14 +39,14 @@ function checkFiles() {
 }
 
 echo "[compile] linux x64";
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" pbf.go;
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 assert $?;
 chmod +x pbf;
 mv pbf build/pbf.linux-x64;
 $SKIP_COMPRESSION || upx --brute build/pbf.linux-x64;
 
 echo "[compile] linux arm";
-GOOS=linux GOARCH=arm go build -ldflags="-s -w" pbf.go;
+GOOS=linux GOARCH=arm go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 echo $?;
 assert $?;
 chmod +x pbf;
@@ -54,70 +54,70 @@ mv pbf build/pbf.linux-arm;
 $SKIP_COMPRESSION || upx --brute build/pbf.linux-arm;
 
 # echo "[compile] linux i386";
-# GOOS=linux GOARCH=386 go build -ldflags="-s -w" pbf.go;
+# GOOS=linux GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.linux-ia32;
 # $SKIP_COMPRESSION || upx --brute build/pbf.linux-ia32;
 
 echo "[compile] darwin x64";
-GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" pbf.go;
+GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 assert $?;
 chmod +x pbf;
 mv pbf build/pbf.darwin-x64;
 $SKIP_COMPRESSION || upx --brute build/pbf.darwin-x64;
 
 # echo "[compile] darwin i386";
-# GOOS=darwin GOARCH=386 go build -ldflags="-s -w" pbf.go;
+# GOOS=darwin GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.darwin-ia32;
 # $SKIP_COMPRESSION || upx --brute build/pbf.darwin-ia32;
 
 echo "[compile] windows x64";
-GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o pbf.exe pbf.go;
+GOOS=windows GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -o pbf.exe pbf.go;
 assert $?;
 chmod +x pbf.exe;
 mv pbf.exe build/pbf.win32-x64;
 $SKIP_COMPRESSION || upx --brute build/pbf.win32-x64;
 
 # echo "[compile] windows i386";
-# GOOS=windows GOARCH=386 go build -ldflags="-s -w" pbf.go;
+# GOOS=windows GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf.exe;
 # mv pbf.exe build/pbf.win32-ia32;
 # $SKIP_COMPRESSION || upx --brute build/pbf.win32-ia32;
 
 # echo "[compile] freebsd arm";
-# GOOS=freebsd GOARCH=arm go build -ldflags="-s -w" pbf.go;
+# GOOS=freebsd GOARCH=arm go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.freebsd-arm;
 # $SKIP_COMPRESSION || upx --brute build/pbf.freebsd-arm;
 
 # echo "[compile] freebsd x64";
-# GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" pbf.go;
+# GOOS=freebsd GOARCH=amd64 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.freebsd-x64;
 # $SKIP_COMPRESSION || upx --brute build/pbf.freebsd-x64;
 
 # echo "[compile] freebsd i386";
-# GOOS=freebsd GOARCH=386 go build -ldflags="-s -w" pbf.go;
+# GOOS=freebsd GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.freebsd-ia32;
 # $SKIP_COMPRESSION || upx --brute build/pbf.freebsd-ia32;
 
 # echo "[compile] openbsd i386";
-# GOOS=openbsd GOARCH=386 go build -ldflags="-s -w" pbf.go;
+# GOOS=openbsd GOARCH=386 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.openbsd-ia32;
 # $SKIP_COMPRESSION || upx --brute build/pbf.openbsd-ia32;
 
 # echo "[compile] openbsd x64";
-# GOOS=openbsd GOARCH=amd64 go build -ldflags="-s -w" pbf.go;
+# GOOS=openbsd GOARCH=amd64 go build -ldflags="-s -w" -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH pbf.go;
 # assert $?;
 # chmod +x pbf;
 # mv pbf build/pbf.openbsd-x64;
