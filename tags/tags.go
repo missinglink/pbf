11a2d724e5da4ef4b21310167f9116fdced4d396
Sergey Zelenov <sergey.zelenov@moovel.com>
2019-05-21 17:07:05 +0200

    add more highway tag values to determine streets

--- tags/tags.go
+++ tags/tags.go
@@ -75,5 +75,8 @@ func Highway() map[string]bool {
 	tags["service"] = false
 	tags["tertiary"] = false
 	tags["road"] = false
+	tags["pedestrian"] = false
+	tags["unclassified"] = false
+	tags["living_street"] = false
 	return tags
 }
