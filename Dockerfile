5465c26102be1dcda529bda899b01bf15b96cf4d
missinglink <insomnia@rcpt.at>
2020-02-03 14:35:50 +0100

    xroads: export to CSV format for import into Pelias

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.10-alpine3.8 AS builder
+FROM golang:1.13.7-alpine3.11 AS builder
 
 WORKDIR "$GOPATH/src/github.com/missinglink/pbf"
 
@@ -9,6 +9,8 @@ COPY . "$GOPATH/src/github.com/missinglink/pbf"
 
 RUN go get && go build
 
-FROM alpine:3.8
+FROM alpine:3.11.3
 
 COPY --from=builder /go/src/github.com/missinglink/pbf/pbf /bin/
+
+ENTRYPOINT [ "pbf" ]
--- handler/xroads.go
+++ handler/xroads.go
@@ -1,59 +1,104 @@
 package handler
 
 import (
-	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/missinglink/gosmparse"
+	"github.com/missinglink/pbf/lib"
 )
 
 // Xroads - Xroads
 type Xroads struct {
-	TagWhiteList  map[string]bool
-	WayNames      map[string]string
-	InvertedIndex map[string][]string
-	Mutex         *sync.Mutex
+	Pass                 int64
+	TagWhiteList         map[string]bool
+	IntersectionWaysMask *lib.Bitmask
+	WayNames             map[int64]string
+	NodeMap              map[int64][]int64
+	Coords               map[int64]*gosmparse.Node
+	Mutex                *sync.Mutex
 }
 
 // ReadNode - called once per node
 func (x *Xroads) ReadNode(item gosmparse.Node) {
-	// noop
+	// skip unless this is the second pass over the file
+	if x.Pass != 1 {
+		return
+	}
+
+	if _, ok := x.NodeMap[item.ID]; ok {
+		x.Mutex.Lock()
+		x.Coords[item.ID] = &gosmparse.Node{
+			Lat: item.Lat,
+			Lon: item.Lon,
+		}
+		x.Mutex.Unlock()
+	}
 }
 
 // ReadWay - called once per way
 func (x *Xroads) ReadWay(item gosmparse.Way) {
+	// compute intersections on first pass
+	if x.Pass == 0 {
 
-	// must be valid highway tag
-	if _, ok := x.TagWhiteList[item.Tags["highway"]]; !ok {
-		return
+		// must be valid highway tag
+		if _, ok := x.TagWhiteList[item.Tags["highway"]]; !ok {
+			return
+		}
+
+		// store the way ids in an array with the nodeid as key
+		for _, nodeid := range item.NodeIDs {
+			x.Mutex.Lock()
+			x.NodeMap[nodeid] = appendUnique(x.NodeMap[nodeid], item.ID)
+			x.Mutex.Unlock()
+		}
 	}
 
-	// convert int64 to string
-	var wayIDString = strconv.FormatInt(item.ID, 10)
-
-	// get the best name from the tags
-	if val, ok := item.Tags["addr:street"]; ok {
-		x.Mutex.Lock()
-		x.WayNames[wayIDString] = val
-		x.Mutex.Unlock()
-	} else if val, ok := item.Tags["name"]; ok {
-		x.Mutex.Lock()
-		x.WayNames[wayIDString] = val
-		x.Mutex.Unlock()
-	} else {
-		return
-	}
-
-	// store the way ids in an array with the nodeid as key
-	for _, nodeid := range item.NodeIDs {
-		var nodeIDString = strconv.FormatInt(nodeid, 10)
-		x.Mutex.Lock()
-		x.InvertedIndex[nodeIDString] = append(x.InvertedIndex[nodeIDString], wayIDString)
-		x.Mutex.Unlock()
+	// only store names on second pass to save memory
+	// (after $IntersectionWaysMask has been populated with matches)
+	if x.Pass == 1 && x.IntersectionWaysMask.Has(item.ID) {
+		// get the best name from the tags
+		if val, ok := item.Tags["addr:street"]; ok {
+			x.Mutex.Lock()
+			x.WayNames[item.ID] = strings.TrimSpace(val)
+			x.Mutex.Unlock()
+		} else if val, ok := item.Tags["name"]; ok {
+			x.Mutex.Lock()
+			x.WayNames[item.ID] = strings.TrimSpace(val)
+			x.Mutex.Unlock()
+		} else {
+			return
+		}
 	}
 }
 
 // ReadRelation - called once per relation
 func (x *Xroads) ReadRelation(item gosmparse.Relation) {
 	// noop
+}
+
+// TrimNonIntersections - remove any nodes which have less than two ways
+func (x *Xroads) TrimNonIntersections() {
+	x.Mutex.Lock()
+	for nodeID, wayIDs := range x.NodeMap {
+		if len(wayIDs) < 2 {
+			delete(x.NodeMap, nodeID)
+		} else {
+			// insert all intersection way IDs in mask
+			for _, wayID := range wayIDs {
+				x.IntersectionWaysMask.Insert(wayID)
+			}
+		}
+	}
+	x.Mutex.Unlock()
+}
+
+// https://stackoverflow.com/a/9561388
+func appendUnique(slice []int64, i int64) []int64 {
+	for _, ele := range slice {
+		if ele == i {
+			return slice
+		}
+	}
+	return append(slice, i)
 }
